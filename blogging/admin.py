8d5f62305d1a9e0077c8d2c0ad2f1e5d8117c75b
mmanc125uw <mmanc125@uw.edu>
2020-09-30 10:35:34 -0400

    sp_py230 lesson7 assign, exclud posts in  cat admin

--- blogging/admin.py
+++ blogging/admin.py
@@ -2,5 +2,9 @@
 from django.contrib import admin
 from blogging.models import Post, Category
 
+class CategoryAdmin(admin.ModelAdmin):
+    
+    exclude = ('posts', )
+
 admin.site.register(Post)
-admin.site.register(Category)
+admin.site.register(Category, CategoryAdmin)
