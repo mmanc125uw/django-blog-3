e3cb163b127f1d418a92ff18ad49f063e929d2a3
mmanc125uw <mmanc125@uw.edu>
2020-10-01 10:32:47 -0400

    sp_py230 lesson7 assign, refact and final test

--- blogging/admin.py
+++ blogging/admin.py
@@ -2,9 +2,19 @@
 from django.contrib import admin
 from blogging.models import Post, Category
 
+class CategoryInline(admin.TabularInline):
+    
+    model = Category.posts.through
+
 class CategoryAdmin(admin.ModelAdmin):
     
     exclude = ('posts', )
 
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    
+    inlines = [
+        CategoryInline,
+    ]
+
+admin.site.register(Post, PostAdmin)
 admin.site.register(Category, CategoryAdmin)
